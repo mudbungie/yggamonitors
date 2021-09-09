494d09bd42d2c902fc40f38164081274383de15d
mudbungie <mudbungie@gmail.com>
2021-09-09 00:19:52 -0700

    Add small, synchronous webserver

---
+++ webserver.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from yggamonitors.lib.web.compile_data import check_all_sites
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    statuses: dict = check_all_sites()
+    return statuses
