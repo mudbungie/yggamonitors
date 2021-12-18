d54fe92b0ad1a0c09e8275b829a115155b063f69
mudbungie <mudbungie@gmail.com>
2021-12-18 02:49:28 -0800

    Add other router

--- yggamonitors/fixtures/sites_to_monitor.py
+++ yggamonitors/fixtures/sites_to_monitor.py
@@ -1,6 +1,7 @@
 known_sites = {
     "MODEM": "https://192.168.0.1",
     "ROUTER": "https://192.168.1.1",
+    "WIFI_ROUTER": "http://192.168.1.1",
     "WAP1": "https://192.168.1.10",
     "WAP2": "https://192.168.1.11",
     "ARBITRARY_SITE": "https://iscaliforniaonfire.com",
